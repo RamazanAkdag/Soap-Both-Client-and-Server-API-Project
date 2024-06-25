56c547a5ef9e739eadd126a09148f74bb3abaacd
RamazanAkdag <akdagramazan586@gmail.com>
2024-06-25 15:57:26 +0300

    init commit

---
+++ src/main/java/com/id3/both_client_server_soap_service/BothClientServerSoapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.id3.both_client_server_soap_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BothClientServerSoapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BothClientServerSoapServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/id3/both_client_server_soap_service/BothClientServerSoapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.id3.both_client_server_soap_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BothClientServerSoapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
